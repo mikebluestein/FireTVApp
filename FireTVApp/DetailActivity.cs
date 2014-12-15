5c26a4cb7dc923035bc0857d2b0f5ff50f961ded
Mike Bluestein <mike.bluestein@gmail.com>
2014-12-14 21:15:09 -0500

    Photo viewer app for Fire TV

---
+++ FireTVApp/DetailActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FireTVApp
+{
+    [Activity (Label = "DetailActivity")]			
+    public class DetailActivity : Activity
+    {
+        ImageView imageView;
+
+        int[] images = { 
+            Resource.Drawable.a1, 
+            Resource.Drawable.b1,
+            Resource.Drawable.c1,
+            Resource.Drawable.d1,
+            Resource.Drawable.e1,
+            Resource.Drawable.f1,
+            Resource.Drawable.g1,
+            Resource.Drawable.h1,
+            Resource.Drawable.i1,
+            Resource.Drawable.j1,
+            Resource.Drawable.k1,
+            Resource.Drawable.l1,
+            Resource.Drawable.m1,
+            Resource.Drawable.n1,
+            Resource.Drawable.o1,
+            Resource.Drawable.p1,
+            Resource.Drawable.q1,
+            Resource.Drawable.r1,
+            Resource.Drawable.s1,
+            Resource.Drawable.t1,
+            Resource.Drawable.v1
+        };
+            
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            SetContentView (Resource.Layout.Detail);
+
+            int i = Intent.Extras.GetInt ("position");
+
+            imageView = FindViewById<ImageView> (Resource.Id.DetailImageView);
+            imageView.SetImageResource (images [i]);
+
+        }
+
+        protected override void OnPause ()
+        {
+            base.OnPause ();
+
+            imageView.SetImageResource (0);
+        }
+    }
+}
---
+++ FireTVApp/MainActivity.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FireTVApp
+{
+    [Activity (Label = "FireTVApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            SetContentView (Resource.Layout.Main);
+
+            var imageAdapter = new ImageAdapter (this);
+            var imageListView = FindViewById<GridView> (Resource.Id.Grid);
+            imageListView.Adapter = imageAdapter;
+
+            imageListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => {
+
+                var intent = new Intent(this, typeof(DetailActivity));
+                intent.PutExtra("position", e.Position);
+                StartActivity(intent);
+            };
+        }
+    }
+
+    class ImageAdapter : BaseAdapter
+    {
+        Context context;
+
+        int[] images = { 
+            Resource.Drawable.a, 
+            Resource.Drawable.b,
+            Resource.Drawable.c,
+            Resource.Drawable.d,
+            Resource.Drawable.e,
+            Resource.Drawable.f,
+            Resource.Drawable.g,
+            Resource.Drawable.h,
+            Resource.Drawable.i,
+            Resource.Drawable.j,
+            Resource.Drawable.k,
+            Resource.Drawable.l,
+            Resource.Drawable.m,
+            Resource.Drawable.n,
+            Resource.Drawable.o,
+            Resource.Drawable.p,
+            Resource.Drawable.q,
+            Resource.Drawable.r,
+            Resource.Drawable.s,
+            Resource.Drawable.t,
+            Resource.Drawable.v
+        };
+
+        public ImageAdapter (Context c)
+        {
+            context = c;
+        }
+
+        public override int Count { get { return images.Length; } }
+
+        public override Java.Lang.Object GetItem (int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId (int position)
+        {
+            return 0;
+        }
+
+        public override View GetView (int position, View convertView, ViewGroup parent)
+        {
+
+            var imageView = new ImageView (context);
+
+            imageView.LayoutParameters = new GridView.LayoutParams (300, 300);
+            imageView.SetScaleType (ImageView.ScaleType.CenterInside);
+            imageView.SetPadding (10, 10, 10, 10);
+            imageView.SetImageResource (images [position]);
+
+            return imageView;
+        }
+    }
+}
